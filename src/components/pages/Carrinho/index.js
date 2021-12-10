44e5e86cbea5fc38eb45f6a29d91c37efc2a9564
abne <ba.alqueres@gmail.com>
2021-12-10 16:56:31 -0300

    atualizados produtos do carrinho

--- src/components/pages/Carrinho/index.js
+++ src/components/pages/Carrinho/index.js
@@ -1,35 +1,30 @@
 import { CarrinhoContext } from '../../../ContextApi/CarrinhoContext';
 import { useContext } from 'react';
-import { CardGroup, Card } from 'react-bootstrap'
+import { Card, Container } from 'react-bootstrap'
 import Footer from '../../Footer';
 import Header from '../../Header';
+import style from './style.scss';
 
 const Carrinho = () => {
     const { cartItems } = useContext(CarrinhoContext)
     return (
         <>
-        < Header />
-
-        <CardGroup>
-
-            {cartItems.length > 0 && cartItems.map(item => {
-                return (
-                    <>
-                        <Card bg="warning" style={{ width: '16rem', gap: '3rem' }}>
-                            <div id="cardImg">
-                                <Card.Img variant="top" src={item.imagem} />
-                            </div>
-                            <Card.Body>
-                                <Card.Title>{item.nome}</Card.Title>
-                                <Card.Body className="d-flex flex-column align-items-center">
-                                    <Card.Text> R&#36;{item.preco}</Card.Text>
-                                </Card.Body>
-                            </Card.Body>
-                        </Card>
-                    </>
-                )
-            })}
-            </CardGroup>
+            < Header />
+                <h1>Carrinho</h1>
+            <Container className=" card-container d-flex flex-wrap justify-content-center">
+                    
+                {cartItems.length > 0 && cartItems.map(item => {
+                    return (
+                        <>
+                            <Card id="card-horizontal" className="d-flex flex-row" bg="warning">
+                                    <div><Card.Img className="d-flex flex-row" style={{ width: '5rem', height: '5rem' }} variant="left" src={item.imagem} /></div>
+                                    <div><Card.Title className="d-flex flex-column">{item.nome}</Card.Title></div>
+                                    <div><Card.Text className="d-flex flex-row"> R&#36;{item.preco}</Card.Text></div>
+                            </Card>
+                        </>
+                    )
+                })}
+            </Container>
 
             <Footer />
         </>
